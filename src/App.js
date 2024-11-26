514368ec4bb0416d792c8ad1c586b9630e0ad140
Hamid Entezam <hamident1983@gmail.com>
2024-11-26 11:01:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,32 +5,43 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
-import Home from "./components/Home";
-import Login from "./components/Auth/Login";
-import 'bulma/css/bulma.min.css';
-import { MainProvider } from "./components/context/MainContext";
-import Header from "./components/Header/Header";
-import { UserPage } from "./components/Users/UserPage";
 
+import Home from "./components/Home";
+import LoginForm from "./components/LoginForm"; 
+import LogoutButton from "./components/LogoutButton"; 
+import React, { useState } from "react";
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+ 
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+  };
+
   return (
     <Router>
-      <MainProvider>
-      <Header/>
-        <main>
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/users" exact>
-              <UserPage />
-            </Route>
-            <Route path="/login" exact><Login/></Route>
-            <Redirect to="/" />
-          </Switch>
-        </main>
-      </MainProvider>
+      <main>
+        <Switch>
+          {/* Home Route */}
+          <Route path="/" exact>
+            <Home />
+            {isAuthenticated && <LogoutButton onLogout={handleLogout} />} {/* Show Logout Button if logged in */}
+          </Route>
+
+          {/* Login Route */}
+          <Route path="/login" exact>
+            <LoginForm onLogin={handleLogin} /> {/* Pass login callback */}
+          </Route>
+
+          {/* Redirect to Home */}
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
