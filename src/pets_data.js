7ec868506ce950fe6ae7ac8b85aba9e16fab8390
Catarina Esposito Mendes <espositc@uoguelph.ca>
2024-11-26 20:32:52 -0500

    Edit pet data

--- src/pets_data.js
+++ src/pets_data.js
@@ -6,7 +6,9 @@ const pets = [
         "breed": "Pomeranian",
         "description": "Bumblebee is an irresistibly cute, fluffy 8-year-old Pomeranian with a heartwarming personality! This little bundle of joy is known for his luxurious, soft coat and bright, expressive eyes that shine with curiosity. Bumblebee has a playful, friendly nature and loves being around people, often bringing smiles with his charming antics and gentle bark. Despite his small size, he has a big personality and loves to prance around, exploring his surroundings and making new friends wherever he goes. At 8 years old, Bumblebee is the perfect mix of spunk and calm, making him an ideal companion.",
         "name": "Bumblebee",
-        "photoURL": "https://www.petexpress.com.ph/cdn/shop/articles/Adorable_Pomeranian.png?v=1710229124"
+        "photoURL": "https://www.petexpress.com.ph/cdn/shop/articles/Adorable_Pomeranian.png?v=1710229124",
+        "creator": "user1"
+
     },
     {
         "key": "673410a2360f1a8153278623",
@@ -15,7 +17,8 @@ const pets = [
         "breed": "Golden Retriever",
         "description": "Bella is a sweet, loyal 4-year-old Golden Retriever with a golden coat that shines in the sun. Friendly and gentle, Bella loves to play fetch and go for long walks, but she’s also happy to snuggle up for a movie night. Her calm temperament makes her great with kids, and she’s always ready to make new friends.",
         "name": "Bella",
-        "photoURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0gcUu-D8ElLcFw0TZSvCdBasqs4rG_YesHQm9Rw269lUZeRMN-dPzVZ9VH82U9Ji8OC3XHBke75d21Coy7Cho4Q"
+        "photoURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0gcUu-D8ElLcFw0TZSvCdBasqs4rG_YesHQm9Rw269lUZeRMN-dPzVZ9VH82U9Ji8OC3XHBke75d21Coy7Cho4Q",
+        "creator": "user1"
     }
 ];
 
