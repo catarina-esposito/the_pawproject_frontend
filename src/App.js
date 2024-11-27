eb8aa4f74a3815d4c3cac76b3c74bbae516e7504
Catarina Esposito Mendes <espositc@uoguelph.ca>
2024-11-27 01:04:45 -0500

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ import UpdatePet from "./pets/pages/UpdatePet";
 import Navigation from "./shared/components/Navigation/Navigation";
 import Footer from "./shared/components/Footer/Footer";
 import Login from "./users/pages/Login";
-import { AuthContext } from "./shared/context/auth-context";
+import { MainContext } from "./shared/context/MainContext";
 
 const App = () => {
   // to test the state for user logged in or not edit "useState(true)" or "useState(false)"
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userId, setUserId] = useState(false);
 
   const login = useCallback((uid) => {
@@ -64,7 +64,7 @@ const App = () => {
   }
 
   return (
-    <AuthContext.Provider
+    <MainContext.Provider
       value={{
         isLoggedIn: isLoggedIn,
         userId: userId,
@@ -77,7 +77,7 @@ const App = () => {
         <main>{routes}</main>
         <Footer />
       </Router>
-    </AuthContext.Provider>
+    </MainContext.Provider>
   );
 };
 
