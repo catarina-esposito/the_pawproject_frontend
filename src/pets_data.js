cf1b6e93b81865e2ffc6b6233d9aeedf99c5d57b
Keitra <keitramroberts17@gmail.com>
2024-11-29 22:03:39 -0500

    Added pet cards

--- src/pets_data.js
+++ src/pets_data.js
@@ -4,7 +4,7 @@ const pets = [
         "adoptionStatus": "Available",
         "age": 8,
         "breed": "Pomeranian",
-        "description": "Bumblebee is an irresistibly cute, fluffy 8-year-old Pomeranian with a heartwarming personality! This little bundle of joy is known for his luxurious, soft coat and bright, expressive eyes that shine with curiosity. Bumblebee has a playful, friendly nature and loves being around people, often bringing smiles with his charming antics and gentle bark. Despite his small size, he has a big personality and loves to prance around, exploring his surroundings and making new friends wherever he goes. At 8 years old, Bumblebee is the perfect mix of spunk and calm, making him an ideal companion.",
+        "description": "Bumblebee is an irresistibly cute, fluffy 8-year-old Pomeranian with a heartwarming personality! This little bundle of joy is known for his luxurious, soft coat and bright, expressive eyes that shine with curiosity. Bumblebee has a playful, friendly nature and loves being around people, often bringing smiles with his charming antics and gentle bark.",
         "name": "Bumblebee",
         "photoURL": "https://www.petexpress.com.ph/cdn/shop/articles/Adorable_Pomeranian.png?v=1710229124",
         "creator": "user1"
@@ -18,6 +18,26 @@ const pets = [
         "description": "Bella is a sweet, loyal 4-year-old Golden Retriever with a golden coat that shines in the sun. Friendly and gentle, Bella loves to play fetch and go for long walks, but she’s also happy to snuggle up for a movie night. Her calm temperament makes her great with kids, and she’s always ready to make new friends.",
         "name": "Bella",
         "photoURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0gcUu-D8ElLcFw0TZSvCdBasqs4rG_YesHQm9Rw269lUZeRMN-dPzVZ9VH82U9Ji8OC3XHBke75d21Coy7Cho4Q",
+        "creator": "user1"
+    },
+    {
+        "key": "673410a2360f1a8153278625",
+        "adoptionStatus": "Available",
+        "age": 1,
+        "breed": "Poodle",
+        "description": "Tassle is a playful, intelligent 1-year-old Poodle with a cream-colored coat that bounces as she moves. Full of charm and curiosity, Tassle loves exploring new places and showing off her tricks. With a loyal and affectionate personality, Tassle is the perfect companion for both fun adventures and cozy cuddles.",
+        "name": "Tassle",
+        "photoURL": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dpoodle%2Bskirt&psig=AOvVaw2wzxnWMSWdI2KohWkCLHyH&ust=1733018946297000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLCyz-n8gooDFQAAAAAdAAAAABAE",
+        "creator": "user1"
+    },
+    {
+        "key": "67340fa6360f1a8153278627",
+        "adoptionStatus": "Available",
+        "age": 5,
+        "breed": "Siberian Husky",
+        "description": "Snow is a majestic 5-year-old white Siberian Husky with piercing blue eyes and a spirited personality. Energetic and adventurous, Snow loves long runs and exploring the outdoors. Loyal and intelligent, Snow thrives on companionship and brings warmth and excitement to every moment.",
+        "name": "Snow",
+        "photoURL": "https://www.petexpress.com.ph/cdn/shop/articles/Adorable_Pomeranian.png?v=1710229124",
         "creator": "user1"
     }
 ];
